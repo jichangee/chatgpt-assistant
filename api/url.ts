60eb58d857d79dbb8e0c0af202f292b338eef50f
moxuy <jichangee@gmail.com>
2023-04-28 14:54:46 +0800

    init

--- api/url.ts
+++ api/url.ts
@@ -1,24 +1,23 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 import axios from "axios";
 import { Readability } from "@mozilla/readability";
 import { JSDOM } from "jsdom";
-import { ChatGPTAPI } from "chatgpt";
+import { message2Messages } from "../utils/openAI";
 
-const CHAT_GPT_API_KEY = process.env.API_KEY || ''
+const CHAT_GPT_API_KEY = process.env.API_KEY || "";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const { url = '' } = req.query
-  return res.json({ url })
+  const { url = "" } = req.query;
   if (Array.isArray(url)) {
-    return res.json({ err: 'url must be string' })
+    return res.json({ err: "url must be string" });
   }
   if (!url) {
-    return res.json({ err: 'url cannot be empty' })
+    return res.json({ err: "url cannot be empty" });
   }
-  const chatGPText = await getChatGPTResult(url).catch(err => {
-    return res.json({ err: JSON.stringify(err), params: url })
-  })
-  return res.json({ data: chatGPText })
+  const chatGPText = await getChatGPTResult(url).catch((err) => {
+    return res.json({ err: JSON.stringify(err), params: url });
+  });
+  return res.json({ data: chatGPText });
 }
 
 const getUrlDocument = (url): Promise<any> => {
@@ -31,17 +30,28 @@ const getUrlDocument = (url): Promise<any> => {
   });
 };
 
-const sendMessageToChatGPT = async (msg) => {
-  const api = new ChatGPTAPI({
-    apiKey: CHAT_GPT_API_KEY
-  })
-  const res = await api.sendMessage(msg)
-  return res.text
-}
+const sendMessageToChatGPT = async (message) => {
+  const messages = message2Messages(message);
+  return axios({
+    url: "https://api.openai.com/v1/chat/completions",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${CHAT_GPT_API_KEY}`,
+    },
+    method: "POST",
+    data: {
+      model: "gpt-3.5-turbo",
+      messages,
+      temperature: 0.6,
+    },
+  }).then((res) => {
+    return res.data.choices[0].message.content;
+  });
+};
 
 async function getChatGPTResult(url: string) {
   try {
-    const text = await getUrlDocument(url)
+    const text = await getUrlDocument(url);
     const doc = new JSDOM(text, {
       url,
       contentType: "text/html",
@@ -51,12 +61,14 @@ async function getChatGPTResult(url: string) {
     const dom = doc.window.document;
     const article = new Readability(dom).parse();
     if (article) {
-      const textContent = article.textContent
-      const chatGPTText = await sendMessageToChatGPT(`Please summarize this article in chinese. \n ` + textContent)
-      return chatGPTText
+      const textContent = article.textContent;
+      const chatGPTText = await sendMessageToChatGPT(
+        `Please summarize this article in chinese. \n ` + textContent
+      );
+      return chatGPTText;
     }
-    return Promise.reject(new Error('article is null'))
+    return Promise.reject(new Error("article is null"));
   } catch (error) {
-    return Promise.reject(new Error(error))
+    return Promise.reject(new Error(error));
   }
 }
---
+++ utils/openAI.ts
@@ -0,0 +1,3 @@
+export const message2Messages = (message) => {
+  return [{"role": "user", "content": message}]
+}
